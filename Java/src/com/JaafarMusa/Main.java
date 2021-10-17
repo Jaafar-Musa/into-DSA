d805847ed1dab963f19eb0262c2aec880381f297
Jaafar Musa <jaafarmusa@Jaafars-MacBook-Pro.local>
2021-10-17 16:16:11 +0000

    java dynamicArray1

---
+++ Java/src/com/JaafarMusa/Main.java
@@ -0,0 +1,13 @@
+package com.JaafarMusa;
+
+
+import com.JaafarMusa.Scripts.DynamicArray1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        DynamicArray1 arr = new DynamicArray1(5);
+        arr.add(1);
+    }
+}
